7faca1c57dae85f1737f71d2a6358d6c2219417a
rahul <rahulxyz123u@gmail.com>
2025-07-16 14:40:13 +0530

    feat: close popup after copy/visit + add toast for copy success

--- components/PopUpLiveSite.tsx
+++ components/PopUpLiveSite.tsx
@@ -7,6 +7,7 @@ import {
 import { Copy, SquareArrowOutUpRight, X } from "lucide-react";
 import { useMemo } from "react";
 import Image from "next/image";
+import { toast } from "sonner";
 
 const PopUpLiveSite = ({
   isOpen,
@@ -19,23 +20,41 @@ const PopUpLiveSite = ({
 }) => {
   const mainContent = useMemo(() => {
     return (
-      <div className="bg-white rounded-lg shadow-lg">
-        <div className="flex flex-col items-center justify-between p-6 gap-5">
+      <div className="rounded-lg bg-white shadow-lg">
+        <div className="flex flex-col items-center justify-between gap-5 p-6">
           <Image
             src="/site-live.svg"
             alt="Site live icon"
             height={41}
             width={52}
           />
-          <h3 className="text-2xl font-medium text-black/95">Your website is live now</h3>
-          <div className="flex flex-col w-full gap-5 md:gap-2">
-            <div className="flex-grow bg-gray-100 border border-text-gray-300 rounded-md p-2 px-3 text-sm text-gray-700 min-h-10">{websiteUrl}</div>
+          <h3 className="text-2xl font-medium text-black/95">
+            Your website is live now
+          </h3>
+          <div className="flex w-full flex-col gap-5 md:gap-2">
+            <div className="border-text-gray-300 min-h-10 flex-grow rounded-md border bg-gray-100 p-2 px-3 text-sm text-gray-700">
+              {websiteUrl}
+            </div>
             <div className="grid grid-cols-2 gap-4 md:gap-2">
-              <button className="bg-black/95 rounded-md hover:bg-gray-800 p-2 text-white flex flex-row gap-2 items-center justify-center">
+              <button
+                onClick={() => {
+                  navigator.clipboard.writeText(websiteUrl);
+                  toast.success("Copy link to you website");
+                  onClose()
+                }}
+                title="Copy Url"
+                className="flex flex-row items-center justify-center gap-2 rounded-md bg-black/95 p-2 text-white hover:bg-gray-800"
+              >
                 <Copy className="size-5" />
                 <span>Copy Url</span>
               </button>
-              <a href={websiteUrl} target="_blank" rel="noreferrer" className="bg-black/95 rounded-md hover:bg-gray-800 p-2 text-white flex flex-row gap-2 items-center justify-center">
+              <a
+                href={websiteUrl}
+                target="_blank"
+                rel="noreferrer"
+                onClick={() => onClose()}
+                className="flex flex-row items-center justify-center gap-2 rounded-md bg-black/95 p-2 text-white hover:bg-gray-800"
+              >
                 <SquareArrowOutUpRight className="size-5" />
                 <span>visite Site</span>
               </a>
@@ -47,12 +66,12 @@ const PopUpLiveSite = ({
   }, [websiteUrl]);
   return (
     <Dialog open={isOpen} onOpenChange={(open) => !open && onClose()}>
-        <DialogHeader>
-          <DialogTitle>
-            <span className="sr-only">Site live</span>
-          </DialogTitle>
-        </DialogHeader>
-      <DialogContent className="sm:maw-w-md p-0 gap-0 border-none h-[280px] w-[500px]">
+      <DialogHeader>
+        <DialogTitle>
+          <span className="sr-only">Site live</span>
+        </DialogTitle>
+      </DialogHeader>
+      <DialogContent className="sm:maw-w-md h-[280px] w-[500px] gap-0 border-none p-0">
         <button className="absolute top-4 right-4 text-gray-400 hover:text-gray-500">
           <X className="size-4" />
         </button>
